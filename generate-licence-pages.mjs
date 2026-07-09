45cb885a0e57897bc2caf0995e018988e16934fb
kadotaku <pillet.vincent@orange.fr>
2026-07-10 00:46:47 +0200

    Add newly active licence pages

--- generate-licence-pages.mjs
+++ generate-licence-pages.mjs
@@ -83,6 +83,10 @@ function escapeHtml(value) {
     .replace(/"/g, "&quot;");
 }
 
+function escapeXml(value) {
+  return escapeHtml(value).replace(/'/g, "&apos;");
+}
+
 function slugLicence(licence) {
   return String(licence || "")
     .trim()
@@ -616,7 +620,7 @@ const sitemap = [
   "  </url>",
   ...sitemapLicences.flatMap((licence) => [
     "  <url>",
-    `    <loc>${licenceUrl(licence)}</loc>`,
+    `    <loc>${escapeXml(licenceUrl(licence))}</loc>`,
     "  </url>",
   ]),
   "</urlset>",
